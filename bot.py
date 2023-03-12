9f72bd8ed056c95dcdc236c88477ce978a53ca8a
LambdaYH <chenyihang1998@outlook.com>
2023-03-12 21:09:40 +0800

    chore: bot.py中移除插件加载

--- bot.py
+++ bot.py
@@ -7,14 +7,6 @@ driver = nonebot.get_driver()
 driver.register_adapter(Adapter)
 config = driver.config
 
-# core
-nonebot.load_plugin("nonebot_plugin_apscheduler")
-nonebot.load_plugin("nonebot_plugin_htmlrender")
-nonebot.load_plugin("nonebot_plugin_imageutils")
-nonebot.load_plugin("nonebot_plugin_datastore")
-nonebot.load_plugins("migang/core/core_plugins")
-
-# plugins
 nonebot.load_from_toml("pyproject.toml")
 
 if __name__ == "__main__":
