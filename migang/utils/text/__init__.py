1939d1e2dc4348e01e631d912151c480733cbf39
LambdaYH <chenyihang1998@outlook.com>
2023-03-22 23:52:08 +0800

    feat: 缝合俄罗斯轮盘

--- migang/utils/text/__init__.py
+++ migang/utils/text/__init__.py
@@ -1,1 +1,27 @@
+from typing import Union
+
 from .filter import filt_message
+
+
+def is_number(s: Union[int, str]) -> bool:
+    """
+    说明:
+        检测 s 是否为数字
+    参数:
+        :param s: 文本
+    """
+    if isinstance(s, int):
+        return True
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    try:
+        import unicodedata
+
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
