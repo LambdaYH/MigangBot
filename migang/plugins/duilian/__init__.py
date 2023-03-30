4893f2b15d1dca3e0e2f1af0c3a27a3a64e1b656
LambdaYH <chenyihang1998@outlook.com>
2023-03-30 22:16:47 +0800

    feat(对联): 添加对联插件

---
+++ migang/plugins/duilian/__init__.py
@@ -0,0 +1,43 @@
+import random
+
+import aiohttp
+from nonebot import on_startswith
+from nonebot.plugin import PluginMetadata
+from nonebot.params import Startswith, EventPlainText
+
+__plugin_meta__ = PluginMetadata(
+    name="对联",
+    description="生成对联",
+    usage=f"""
+指令：
+    /duilian 上联
+由https://ai.binwang.me/couplet提供
+""".strip(),
+    extra={
+        "unique_name": "migang_duilian",
+        "example": "",
+        "author": "migang",
+        "version": "0.0.1",
+    },
+)
+
+duilian = on_startswith("/duilian", priority=5, block=True)
+
+
+async def get_xialian(shanglian: str):
+    async with aiohttp.ClientSession() as client:
+        r = await client.get(
+            f"https://seq2seq-couplet-model.rssbrain.com/v0.2/couplet/{shanglian}",
+            timeout=15,
+        )
+        r = await r.json()
+    if len(r["output"]) == 0:
+        return ""
+    return random.choice([i for i in r["output"] if i != ""])
+
+
+@duilian.handle()
+async def _(text: str = EventPlainText(), cmd: str = Startswith()):
+    shanglian = text.removeprefix(cmd).strip()
+    xialian = await get_xialian(shanglian)
+    await duilian.send(f"上联：{shanglian}\n下联：{xialian}")
