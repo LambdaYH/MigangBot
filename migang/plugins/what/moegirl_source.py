f65ac1a1a44f45ce345c7b1994ea0164979fc9d8
LambdaYH <chenyihang1998@outlook.com>
2023-04-03 21:43:10 +0800

    fix(what): 歧义页直接不作为查询结果

--- migang/plugins/what/moegirl_source.py
+++ migang/plugins/what/moegirl_source.py
@@ -30,12 +30,15 @@ async def get_moegirl(keyword: str) -> Tuple[str, str, str]:
             info_box = info_box[0]
             msg = info_box.xpath("./following::p[1]")[0].xpath("string(.)").strip("\n")
         else:
-            # 可能是不明确条目，eg. https://zh.moegirl.org.cn/%E6%9A%96%E6%9A%96
-            ele = dom.xpath(
-                "//div[@class='mw-parser-output']/*[not(self::table[@id='disambigbox'])]"
-            )
-            msg = "".join([e.xpath("string(.)") for e in ele]).strip("\n")
-            msg = re.sub(r"\n\n+", "\n\n", msg)
+            return "", "", None
+            # # 可能是不明确条目，eg. https://zh.moegirl.org.cn/%E6%9A%96%E6%9A%96
+            # ele = dom.xpath("//div[@class='mw-parser-output']")
+            # msg = ele[0].xpath("string(.)")
+            # msg = msg.replace(
+            #     f"这是一个消歧义页，罗列了有相同或相近的标题，但内容不同的条目。如果有未列出的条目，欢迎添加。您也可以搜索标题包含【{title}】的条目 或 全站搜索【{title}】。如果您是通过某个无关条目的内部链接而转到本页，希望您能协助修正该处的内部链接，将它指向正确的条目。",
+            #     "",
+            # ).strip("\n")
+            # msg = re.sub(r"\n\n+", "\n\n", msg)
         msg = msg.strip()
         if len(msg) >= 10000 or not msg:
             # 太长了可能爬错了
