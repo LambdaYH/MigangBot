37984d139a2791222d3333ab548cc513cf2f917e
LambdaYH <chenyihang1998@outlook.com>
2023-04-10 17:24:42 +0800

    style: clean code

--- migang/core/core_plugins/chat/chatgpt/extensions/paint.py
+++ migang/core/core_plugins/chat/chatgpt/extensions/paint.py
@@ -1,36 +1,11 @@
-import os
-import uuid
-
 import openai
 from transformers import GPT2TokenizerFast
 
 tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
 
-import anyio
-import requests
-
 from migang.core import get_config
 
 from ..extension import extension_manager
-
-# 拓展的配置信息，用于ai理解拓展的功能 *必填*
-ext_config: dict = {
-    "name": "paint",  # 拓展名称，用于标识拓展
-    "arguments": {
-        "content": "str",  # 绘画内容描述
-    },
-    "description": "paint a picture，使用/#paint&CONTENT#/，其中CONTENT是用逗号分隔的描述性词语。(例如：/#paint&兔子,草地,彩虹#/)",
-    # 参考词，用于上下文参考使用，为空则每次都会被参考(消耗token)
-    "refer_word": ["paint", "画", "图"],
-    # 每次消息回复中最大调用次数，不填则默认为99
-    "max_call_times_per_msg": 3,
-    # 作者信息
-    "author": "OREOREO",
-    # 版本
-    "version": "0.0.1",
-    # 拓展简介
-    "intro": "绘图",
-}
 
 
 @extension_manager(
