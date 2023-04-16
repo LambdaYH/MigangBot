51960c47b63bb7af38624fce908b997d97fc25d5
LambdaYH <chenyihang1998@outlook.com>
2023-04-16 15:15:36 +0800

    feat(joke): 缝合笑话插件

---
+++ migang/plugins/joke/__init__.py
@@ -0,0 +1,48 @@
+"""
+https://github.com/SAGIRI-kawaii/sagiri-bot/tree/Ariadne-v4/modules/self_contained/joke
+"""
+import random
+from pathlib import Path
+from typing import Tuple, Annotated
+
+from nonebot import on_regex
+from nonebot.params import RegexGroup
+from nonebot.plugin import PluginMetadata
+
+from migang.utils.file import async_load_data
+
+__plugin_meta__ = PluginMetadata(
+    name="笑话",
+    description="生成笑话",
+    usage="""
+usage：
+    指令：
+        来点笑话
+        来点(关键词/法国/苏联/美国)笑话
+    示例：
+        来点苏联笑话
+        来自xx笑话
+    说明：
+        关键词不超过五个字
+""".strip(),
+    extra={
+        "unique_name": "migang_joke",
+        "example": "",
+        "author": "migang",
+        "version": 0.1,
+    },
+)
+
+joke = on_regex(r"^来点([\S\s]{1,5})笑话$", priority=5, block=True)
+
+joke_path = Path(__file__).parent / "jokes.json"
+
+
+@joke.handle()
+async def _(reg_group: Annotated[Tuple, RegexGroup()]):
+    data = await async_load_data(joke_path)
+    if reg_group[0] not in data:
+        msg = random.choice(data["jokes"]).format(name=reg_group[0])
+    else:
+        msg = random.choice(data[reg_group[0]])
+    await joke.send(msg)
