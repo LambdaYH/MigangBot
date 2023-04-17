87149dbb50abe9451d90fc957980cfc66361f5af
LambdaYH <chenyihang1998@outlook.com>
2023-04-17 16:18:38 +0800

    feat(exchange_rate_conversion): 汇率转换插件，直接从谷歌搜索截图

---
+++ migang/plugins/exchange_rate_conversion/__init__.py
@@ -0,0 +1,77 @@
+import urllib.parse
+from typing import Annotated
+
+from nonebot.params import CommandArg
+from nonebot import require, on_command
+from nonebot.plugin import PluginMetadata
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+
+from migang.utils.text import is_number
+from migang.core import ConfigItem, get_config
+
+require("nonebot_plugin_htmlrender")
+from nonebot_plugin_htmlrender import get_new_page
+
+__plugin_hidden__ = True
+__plugin_meta__ = PluginMetadata(
+    name="汇率转换",
+    description="快速查询汇率转换",
+    usage="""
+直接从Google获取的结果
+指令：
+    汇率转换 from:to 数额
+示例：
+    汇率转换 usd:cny 5 表示查询5usd转换成的人民币数额
+    汇率转换 eur 5
+说明：
+    当to为空时，默认为cny
+""".strip(),
+    extra={
+        "unique_name": "migang_exchange_rate_conversion",
+        "example": "",
+        "author": "migang",
+        "version": 0.1,
+    },
+)
+
+__plugin_category__ = "一些工具"
+__plugin_config__ = ConfigItem(
+    "proxy", initial_value=None, default_value=None, description="代理服务器"
+)
+
+
+exchange = on_command("汇率转换", priority=5, block=False)
+
+
+@exchange.handle()
+async def _(args: Annotated[Message, CommandArg()]):
+    args = args.extract_plain_text().split(" ")
+    if len(args) != 2:
+        await exchange.finish(f"格式错误，请按照【汇率转换 from:to 数额】发送")
+    from_to = args[0].split(":")
+    if len(from_to) == 1:
+        from_ = from_to[0]
+        to_ = "cny"
+    elif len(from_to) == 2:
+        from_, to_ = from_to[0], from_to[1]
+    else:
+        await exchange.finish(f"输入转换的货币必须为 from:to")
+    if not is_number(args[1]):
+        await exchange.finish("用于转换的数额必须为数字")
+    params = {}
+    if proxy := await get_config("proxy"):
+        params["proxy"] = {"server": proxy}
+    try:
+        async with get_new_page(**params) as page:
+            await page.goto(
+                f"https://www.google.com/search?{urllib.parse.urlencode({'q': f'{args[1]}+{from_}+to+{to_}'})}",
+                wait_until="networkidle",
+                timeout=10 * 1000,
+            )
+            card = await page.query_selector(
+                "div[data-attrid='Converter']",
+            )
+            img = await card.screenshot()
+    except Exception:
+        await exchange.finish(f"似乎无法实现这种货币的转换呢...试试换个输入吧")
+    await exchange.send(MessageSegment.image(img))
