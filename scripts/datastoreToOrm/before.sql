a142baf9591ca18fca0c701f05208169952307a9
LambdaYH <chenyihang1998@outlook.com>
2024-02-04 21:51:58 -0500

    fix(sql): 迁移脚本修复

--- scripts/datastoreToOrm/before.sql
+++ scripts/datastoreToOrm/before.sql
@@ -1,17 +1,14 @@
+-- chatrecord --
+ALTER TABLE "public"."nonebot_plugin_chatrecorder_messagerecord" RENAME TO "nonebot_plugin_chatrecorder_messagerecord_old";
+DROP TABLE IF EXISTS "public"."nonebot_plugin_chatrecorder_alembic_version";
+
 -- session --
 ALTER TABLE "public"."nonebot_plugin_session_sessionmodel" RENAME CONSTRAINT "unique_session" TO "unique_session_old";
 
 -- wordcloud --
 ALTER TABLE "public"."nonebot_plugin_wordcloud_schedule" RENAME TO "nonebot_plugin_wordcloud_schedule_old";
-ALTER TABLE "public"."nonebot_plugin_wordcloud_schedule_old" DROP CONSTRAINT "pk_nonebot_plugin_wordcloud_schedule";
 DROP TABLE IF EXISTS "public"."nonebot_plugin_wordcloud_alembic_version";
 
 -- shidan --
 ALTER TABLE "public"."nonebot_plugin_shindan_shindanrecord" RENAME TO "nonebot_plugin_shindan_shindanrecord_old";
-ALTER TABLE "public"."nonebot_plugin_shindan_shindanrecord_old" DROP CONSTRAINT "pk_nonebot_plugin_shindan_shindanrecord";
 DROP TABLE IF EXISTS "public"."nonebot_plugin_shindan_alembic_version";
-
--- chatrecord --
-ALTER TABLE "public"."nonebot_plugin_chatrecorder_messagerecord" RENAME TO "nonebot_plugin_chatrecorder_messagerecord_old";
-ALTER TABLE "public"."nonebot_plugin_chatrecorder_messagerecord_old" DROP CONSTRAINT "pk_nonebot_plugin_chatrecorder_messagerecord";
-DROP TABLE IF EXISTS "public"."nonebot_plugin_chatrecorder_alembic_version";
