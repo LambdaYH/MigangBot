af2db9a91fd7520cb988f74d7871ef04f21e0e18
LambdaYH <chenyihang1998@outlook.com>
2024-02-06 06:51:22 -0500

    fix: 修复迁移后的序列问题

--- scripts/datastoreToOrm/post.sql
+++ scripts/datastoreToOrm/post.sql
@@ -13,3 +13,12 @@ DROP TABLE IF EXISTS "public"."nonebot_plugin_session_alembic_version";
 -- wordcloud --
 INSERT INTO "nonebot_plugin_wordcloud_schedule" SELECT id, target, time from nonebot_plugin_wordcloud_schedule_old;
 DROP TABLE IF EXISTS "public"."nonebot_plugin_wordcloud_schedule_old";
+
+-- 更新自增序列 --
+SELECT setval('nonebot_plugin_session_orm_sessionmodel_id_seq', (SELECT max(id) from nonebot_plugin_session_orm_sessionmodel));
+
+SELECT setval('nonebot_plugin_chatrecorder_messagerecord_id_seq1', (SELECT max(id) from nonebot_plugin_chatrecorder_messagerecord));
+
+SELECT setval('nonebot_plugin_shindan_shindanrecord_id_seq1', (SELECT max(id) from nonebot_plugin_shindan_shindanrecord));
+
+SELECT setval('nonebot_plugin_wordcloud_schedule_id_seq1', (SELECT max(id) from nonebot_plugin_wordcloud_schedule));
