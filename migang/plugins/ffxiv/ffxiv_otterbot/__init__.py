828ecb1c0274ee67b5d84f480ef91b63ec26fd2b
LambdaYH <chenyihang1998@outlook.com>
2024-07-25 21:16:49 +0800

    feat: 獭窝转发

---
+++ migang/plugins/ffxiv/ffxiv_otterbot/__init__.py
@@ -0,0 +1,74 @@
+from typing import Union
+import websockets
+from websockets.legacy.client import Connect
+from websockets.exceptions import ConnectionClosedError
+
+from nonebot.log import logger
+from nonebot import on_message
+from nonebot.plugin import PluginMetadata
+from nonebot.adapters.onebot.v11 import (
+    Bot,
+    MessageEvent,
+    MessageSegment,
+    GroupMessageEvent,
+    PrivateMessageEvent,
+)
+import asyncio
+from migang.core import ConfigItem, get_config
+from nonebot import get_driver, on_message
+from nonebot.adapters import Event
+from nonebot.drivers import WebSocketClientMixin, Driver, Request
+from .websocket import WebSocketConn
+
+__plugin_meta__ = PluginMetadata(
+    name="otterbot",
+    description="连接到獭窝",
+    usage="""
+usage：
+    将一些指令转发到獭窝
+""".strip(),
+    type="application",
+    supported_adapters={"~onebot.v11"},
+)
+__plugin_category__ = "FF14"
+__plugin_hidden__ = True
+
+__plugin_config__ = (
+    ConfigItem(
+        key="url",
+        initial_value="wss://xn--v9x.net/ws",
+        description="獭窝地址，主窝wss://xn--v9x.net/ws",
+    ),
+    ConfigItem(
+        key="bot_id",
+        description="獭窝中配置的bot_id",
+    ),
+    ConfigItem(
+        key="access_token", description="token"
+    ),
+)
+
+driver: Driver = get_driver()
+
+ws_conn: WebSocketConn
+
+@driver.on_bot_connect
+async def setup_ws(bot: Bot):
+    access_token = await get_config("access_token")
+    if not access_token:
+        return
+    bot_id = await get_config("bot_id")
+    if not bot_id:
+        bot_id = bot.self_id
+    url = await get_config("url")
+    if not url:
+        return
+    global ws_conn
+    ws_conn = WebSocketConn(bot=bot, url = url, bot_id=bot_id, access_token=access_token)
+    asyncio.gather(ws_conn.connect())
+
+handle_message = on_message(block=False)
+
+@handle_message.handle()
+async def _(event: MessageEvent):
+    await ws_conn.forwardEvent(event)
---
+++ migang/plugins/ffxiv/ffxiv_otterbot/websocket.py
@@ -0,0 +1,126 @@
+from asyncio import Queue
+from typing import Any
+
+from nonebot.adapters import Event, Bot
+from websockets import WebSocketClientProtocol
+from websockets.exceptions import ConnectionClosedError, ConnectionClosedOK
+from nonebot.log import logger
+import websockets
+import asyncio
+import ujson
+import time
+from pydantic import BaseModel
+
+_HEARTBEAT_INTERVAL = 3
+
+def _get_heartbeat_event(bot_id: int) -> str:
+    data = {
+        "post_type": "meta_event",
+        "meta_event_type": "heartbeat",
+        "interval": _HEARTBEAT_INTERVAL,
+        "status": {
+            "online":True,
+            "good":True}
+        ,"self_id": bot_id,
+        "time": int(time.time())
+    }
+    return ujson.dumps(data)
+
+def _build_ret_msg(data) -> dict[str, Any]:
+    return {
+        "status": "ok",
+        "retcode": 0,
+        "data": data
+    }
+
+class WebSocketConn:
+
+    def __init__(self, bot: Bot, url: str, bot_id: int, access_token: str) -> None:
+        self.__queue = Queue()
+        self.__url = url
+        self.__bot_id = bot_id
+        self.__access_token = access_token
+        self.__bot = bot
+        self.__send_task = None
+        self.__recv_task = None
+        self.__stop_flag = False
+        self.__websocket: WebSocketClientProtocol = None
+
+    async def connect(self):
+        self.__stop_flag = True
+        while self.__stop_flag:
+            try:
+                async with websockets.connect(
+                    self.__url,
+                    extra_headers={
+                        "Authorization": f"Bearer {self.__access_token}",
+                        "X-Self-ID": self.__bot_id,
+                        "X-Client-Role": "Universal",
+                        "User-Agent": "OneBot",
+                    },
+                ) as websocket:
+                    logger.info("与獭窝已成功建立连接")
+                    self.__websocket = websocket
+                    self.__send_task = asyncio.create_task(self.__ws_send(websocket))
+                    self.__recv_task = asyncio.create_task(self.__ws_recv(websocket))
+                    self.__heartbeat = asyncio.create_task(self.__send_heartbeat(websocket))
+                    await asyncio.gather(self.__send_task, self.__recv_task, self.__heartbeat)
+            except (ConnectionClosedError, ConnectionClosedOK):
+                logger.opt(colors=True).warning(
+                    "<y><bg #f8bbd0>WebSocket Closed</bg #f8bbd0></y>"
+                )
+                self.__send_task.cancel()
+                self.__recv_task.cancel()
+                await asyncio.sleep(1)  # 等待重连
+
+    async def __send_heartbeat(self, ws: WebSocketClientProtocol):
+        while self.__stop_flag:
+            try:
+                await asyncio.sleep(_HEARTBEAT_INTERVAL)
+                await ws.send(_get_heartbeat_event(self.__bot_id))
+            except Exception as e:
+                logger.error(f"发送心跳事件失败：{e}")
+    
+    async def stop(self):
+        self.__stop_flag = False
+        self.__websocket.close()
+
+    async def forwardEvent(self, event: Event):
+        if hasattr(event, "self_id"):
+            event.self_id = self.__bot_id
+        await self.__queue.put(event)
+
+
+    async def _call_api(self, data: dict[str, Any]) -> Any:
+        echo = data.get("echo", "")
+        try:
+            action = data["action"]
+            if action == "get_group_member_list":
+                data["params"]["group_id"] = str(data["params"]["group_id"])
+            resp = await self.__bot.call_api(data["action"], **data["params"])
+            resp_data = _build_ret_msg(resp)
+            if echo:
+                resp_data["echo"] = echo
+            await self.__queue.put(resp_data)
+        except Exception as e:
+            logger.warning(f"调用api失败：{data}：{e}")
+            return {"status": "failed", "echo": echo}
+
+    async def __ws_send(self, ws: WebSocketClientProtocol):
+        while self.__stop_flag:
+            event = await self.__queue.get()
+            send_data: str
+            if isinstance(event, BaseModel):
+                send_data = event.model_dump()
+                send_data["message"] = send_data["raw_message"]
+                send_data = ujson.dumps(send_data)
+            elif isinstance(event, dict):
+                send_data = ujson.dumps(event)
+            elif isinstance(event, str):
+                send_data = event
+            await ws.send(send_data)
+
+    async def __ws_recv(self, ws: WebSocketClientProtocol):
+        while self.__stop_flag:
+            raw_data = await ws.recv()
+            await self._call_api(ujson.loads(raw_data))
