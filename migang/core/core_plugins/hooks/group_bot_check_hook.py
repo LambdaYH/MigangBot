6a0c5f56e4f0cae336561a732beffd89d77ac657
LambdaYH <chenyihang1998@outlook.com>
2024-08-14 19:01:36 +0800

    feat: 当at时无视群机器人设置

--- migang/core/core_plugins/hooks/group_bot_check_hook.py
+++ migang/core/core_plugins/hooks/group_bot_check_hook.py
@@ -4,6 +4,8 @@ from nonebot import get_bots
 from nonebot.exception import IgnoredException
 from nonebot.message import event_preprocessor
 from nonebot.adapters.onebot.v11 import (
+    Bot,
+    MessageSegment,
     GroupMessageEvent,
     GroupBanNoticeEvent,
     GroupAdminNoticeEvent,
@@ -16,9 +18,8 @@ from migang.core.manager import group_bot_manager
 
 
 @event_preprocessor
-async def _(
+async def check_group_bot(
     event: Union[
-        GroupMessageEvent,
         GroupAdminNoticeEvent,
         GroupBanNoticeEvent,
         GroupDecreaseNoticeEvent,
@@ -31,3 +32,23 @@ async def _(
         raise IgnoredException("躺~")
     if event.get_user_id() in get_bots():
         raise IgnoredException("发抖")
+
+
+@event_preprocessor
+async def _(
+    bot: Bot,
+    event: GroupMessageEvent,
+):
+    # 过滤机器人发的消息
+    if event.get_user_id() in get_bots():
+        raise IgnoredException("发抖")
+    # 检查群机器人
+    if group_bot_manager.check_group_bot(event.group_id, event.self_id):
+        # 如果是群机器人，直接响应
+        return
+    # 如果不是群机器人，检查是否@自己，喊名字的不响应，@的响应
+    if event.is_tome():
+        for seg in event.original_message:
+            if seg.type == "at" and seg.data.get("qq") == bot.self_id:
+                return
+    raise IgnoredException("躺~")
