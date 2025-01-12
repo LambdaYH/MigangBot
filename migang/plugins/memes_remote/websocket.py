3c60fc48e1474467158f93a2f0342fc4db407bf1
LambdaYH <chenyihang1998@outlook.com>
2025-01-12 17:18:44 +0800

    fix: 添加task_done

--- migang/plugins/memes_remote/websocket.py
+++ migang/plugins/memes_remote/websocket.py
@@ -184,6 +184,8 @@ class WebSocketConn:
                 raise e
             except Exception as e:
                 logger.error(f"发送memes信息异常：{e}")
+            finally:
+                self.__queue.task_done()
 
     async def __ws_recv(self, ws: WebSocketClientProtocol):
         while self.__connect:
