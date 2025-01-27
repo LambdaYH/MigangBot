43069d2965fc212b5bd7a41c890913da6804c13b
LambdaYH <chenyihang1998@outlook.com>
2025-01-27 08:57:53 +0800

    fix: 优化

--- migang/utils/date/__init__.py
+++ migang/utils/date/__init__.py
@@ -11,11 +11,11 @@ def is_chinese_new_year():
     return lunar_today.month == 1 and lunar_today.day == 1
 
 
-def is_new_year():
+def is_solor_new_year():
     today = datetime.now()
 
-    lunar_today = Converter.Solar2Lunar(Solar(today.year, today.month, today.day))
+    return today.month == 1 and today.day == 1
 
-    return (lunar_today.month == 1 and lunar_today.day == 1) or (
-        today.month == 1 and today.day == 1
-    )
+
+def is_new_year():
+    return is_chinese_new_year() or is_solor_new_year()
