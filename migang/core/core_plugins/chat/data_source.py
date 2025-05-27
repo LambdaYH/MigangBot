5926bca072b7fb48a05d03fc38ac112aac5859d7
LambdaYH <chenyihang1998@outlook.com>
2025-05-27 15:13:17 +0800

    refactor: 移除图灵

--- migang/core/core_plugins/chat/data_source.py
+++ migang/core/core_plugins/chat/data_source.py
@@ -2,83 +2,16 @@ import random
 from pathlib import Path
 from datetime import timedelta
 from typing import List, Optional
-from asyncio.exceptions import TimeoutError as AsyncioTimeoutError
 
 import anyio
 import ujson
-import aiohttp
 from nonebot import get_driver
 from nonebot.log import logger
-from nonebot.adapters.onebot.v11 import Message, MessageSegment, GroupMessageEvent
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
 
-from migang.core import get_config
 from migang.core.permission import BLACK
 from migang.core.utils import http_utils
 from migang.core.manager import permission_manager
-
-
-async def get_turing(
-    nickname: str, plain_text: str, event: GroupMessageEvent, user_id: int
-) -> Optional[str]:
-    """获取图灵回复"""
-    img_url = [seg.data["url"] for seg in event.message["image"]]
-    img_url = img_url[0] if img_url else ""
-    rst = await tu_ling(plain_text, img_url, user_id)
-    if not rst:
-        return None
-    rst = rst.replace("小主人", nickname).replace("小朋友", nickname)
-    return rst
-
-
-# 图灵接口
-turing_key_idx = 0
-
-
-async def tu_ling(text: str, img_url: str, user_id: int) -> Optional[str]:
-    """
-    获取图灵接口的回复
-    """
-    global turing_key_idx
-    tl_keys = await get_config("turing_keys")
-    if not tl_keys:
-        return None
-    tl_key = tl_keys[turing_key_idx]
-    req = {
-        "perception": {
-            "inputText": {"text": text},
-            "selfInfo": {
-                "location": {"city": "南极洲", "province": "南极洲", "street": "南极火山烹饪研究所"}
-            },
-        },
-        "userInfo": {"apiKey": tl_key, "userId": str(user_id)},
-    }
-    if text:
-        req["perception"]["inputText"] = {"text": text}
-    if img_url:
-        req["perception"]["inputImage"] = {"url": img_url}
-    text = None
-    try:
-        async with aiohttp.ClientSession(json_serialize=ujson.dumps) as client:
-            async with client.post(
-                url="http://openapi.turingapi.com/openapi/api/v2", json=req, timeout=5
-            ) as response:
-                if response.status != 200:
-                    return None
-                resp_payload = await response.json(content_type=None)
-    except AsyncioTimeoutError as e:
-        logger.warning(f"访问turingapi超时：{e}")
-        return None
-    if int(resp_payload["intent"]["code"]) in [4003]:
-        return ""
-    if resp_payload["results"]:
-        for result in resp_payload["results"]:
-            if result["resultType"] == "text":
-                text = result["values"]["text"]
-                if "请求次数超过" in text:
-                    text = None
-                    turing_key_idx = (turing_key_idx + 1) % len(tl_keys)
-    return text
-
 
 hello_img = [file for file in (Path(__file__).parent / "image" / "zai").iterdir()]
 hello_msg = set(
