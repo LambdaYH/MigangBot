f8eb58c67cca188d6b686e9ad38300896f9e16bc
LambdaYH <chenyihang1998@outlook.com>
2025-11-09 12:51:37 +0800

    fix(zwa): 修复早晚安接口

--- migang/plugins/zwa/__init__.py
+++ migang/plugins/zwa/__init__.py
@@ -38,7 +38,8 @@ MORNING = (
 )
 
 _HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",
+    "Accept-Encoding": "gzip, deflate",
 }
 
 
